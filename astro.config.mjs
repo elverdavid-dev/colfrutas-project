e555071fec096082489710a5fdf934da3d494816
elverdavid <elverdavid0839@gmail.com>
2023-04-26 16:15:34 -0500

    remove analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import {inject} from '@vercel/analytics'
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(),inject()]
+  integrations: [tailwind(), react()]
 });
