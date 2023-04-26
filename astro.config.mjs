e7d979fd6d4b84993fb062f9ba2b307db66f27c8
elverdavid <elverdavid0839@gmail.com>
2023-04-26 15:35:59 -0500

    new analityc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import {inject} from '@vercel/analytics'
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(),inject()]
 });
