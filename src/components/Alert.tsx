9333f051939926afc1c77abadb29534f19f33307
elverdavid <elverdavid0839@gmail.com>
2023-05-08 17:08:57 -0500

    new modific alert

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,10 +1,16 @@
 import { AiOutlineInfoCircle } from "react-icons/ai/index.js";
 import {Alert} from 'flowbite-react'
+import { useState } from "react";
 const AlertMessage = () => {
+  const [closeMessage, setCloseMessage] = useState(false)
+  console.log(closeMessage)
  return (
    <Alert
+   className={`${(closeMessage === true) ? "hidden" : ""}`}
    color="info"
    withBorderAccent={true}
+   onDismiss={()=>{setCloseMessage(!closeMessage)}}
+
    >
     <div className="flex items-cente justify-start gap-x-2">
      <AiOutlineInfoCircle className="text-3xl"/>
