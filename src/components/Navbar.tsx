b7be3336e40872580d29d438bf0fccb1a5d8556e
elverdavid <elverdavid0839@gmail.com>
2023-08-12 12:18:53 -0500

    add new logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,49 +2,48 @@ import { useState } from "react";
 import { RiWhatsappLine, RiMenuFill, RiCloseFill } from "react-icons/ri/index.js";
 
 const Navbar = () => {
- const [open, setOpen] = useState(false);
- return (
-  <>
-   <nav className="Navbar-content">
-    <div className="flex items-center justify-between px-4 lg:px-0">
-     <a href="/">
-      <img src="/logo.png" width="100" height="100" alt="logo colfrutas" />
-     </a>
-     <div className="cursor-pointer" onClick={() => setOpen(!open)}>
-      {open ? <RiCloseFill className="Icons-navbar" /> : <RiMenuFill className="Icons-navbar" />}
-     </div>
-    </div>
-    <div className={`Navbar-content-item ${open ? "top-20" : "top-[-820px] "}`}>
-     <ul
-      className={`Navbar-list ${
-       open ? "top-20 opacity-100 " : "top-[-820px] lg:opacity-100 opacity-0"
-      }`}
-     >
-      <li className="Navbar-item">
-       <a href="/">inicio</a>
-      </li>
-      <li className="Navbar-item">
-       <a href="/nosotros">nosotros</a>
-      </li>
-      <li className="Navbar-item">
-       <a href="/productos">productos</a>
-      </li>
-      <li className="Navbar-item">
-       <a href="/contacto">contacto</a>
-      </li>
-     </ul>
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <nav className="Navbar-content">
+        <div className="flex items-center justify-between px-4 lg:px-0">
+          <a href="/">
+            <img src="/logo.svg" width="50" height="50" alt="logo colfrutas" />
+          </a>
+          <div className="cursor-pointer" onClick={() => setOpen(!open)}>
+            {open ? <RiCloseFill className="Icons-navbar" /> : <RiMenuFill className="Icons-navbar" />}
+          </div>
+        </div>
+        <div className={`Navbar-content-item ${open ? "top-16" : "top-[-820px] "}`}>
+          <ul
+            className={`Navbar-list ${open ? "top-16 opacity-100 " : "top-[-820px] lg:opacity-100 opacity-0"
+              }`}
+          >
+            <li className="Navbar-item">
+              <a href="/">inicio</a>
+            </li>
+            <li className="Navbar-item">
+              <a href="/nosotros">nosotros</a>
+            </li>
+            <li className="Navbar-item">
+              <a href="/productos">productos</a>
+            </li>
+            <li className="Navbar-item">
+              <a href="/contacto">contacto</a>
+            </li>
+          </ul>
 
-     <a
-      className="Button"
-      target="_blank"
-      href="https://api.whatsapp.com/send?phone=573197365616&amp;text=Me%20interesa%20tener%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Colfrutas,%20mi%20nombre%20es"
-     >
-      <RiWhatsappLine /> Whatsapp
-     </a>
-    </div>
-   </nav>
-  </>
- );
+          <a
+            className="Button"
+            target="_blank"
+            href="https://api.whatsapp.com/send?phone=573197365616&amp;text=Me%20interesa%20tener%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Colfrutas,%20mi%20nombre%20es"
+          >
+            <RiWhatsappLine /> Whatsapp
+          </a>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
